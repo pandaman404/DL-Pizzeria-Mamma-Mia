2e1ac13bc0b7c3167be744f7b0b684a46a8a1db1
pandaman404 <valenzuela.ahumada4@gmail.com>
2024-08-26 22:41:06 -0400

    feat: Profile Page

--- src/pages/auth/profile/index.tsx
+++ src/pages/auth/profile/index.tsx
@@ -1,5 +1,62 @@
+import Button from '@/components/ui/Button';
+import Container from '@/components/ui/Container';
+import Flex from '@/components/ui/Flex';
+import RootLayout from '@/components/ui/RootLayout';
+import Title from '@/components/ui/Title';
+import styled from 'styled-components';
+
 const ProfilePage = () => {
-  return <div>index</div>;
+  return (
+    <RootLayout>
+      <ProfileContainer>
+        <Title as='h2'>Perfil</Title>
+        <div className='email-container'>
+          <strong>Email:</strong>
+          <span>ejemplo@gmail.com</span>
+        </div>
+        <LogoutButton onClick={() => console.log('cerrar sesión')}>Cerrar Sesión</LogoutButton>
+      </ProfileContainer>
+    </RootLayout>
+  );
 };
+
+const ProfileContainer = styled(Container)`
+  ${Flex}
+  flex-direction: column;
+  align-items: flex-start;
+  max-height: 300px;
+  max-width: 500px;
+  gap: 2rem;
+  border: 1px solid ${({ theme }) => theme.lightGray};
+  padding: 2rem;
+  border-radius: 10px;
+
+  h2 {
+    font-size: 32px;
+    color: ${({ theme }) => theme.deepBlue};
+    margin-bottom: 2rem;
+    align-self: center;
+  }
+
+  .email-container {
+    ${Flex}
+    gap: 1rem;
+
+    strong {
+      color: ${({ theme }) => theme.deepBlue};
+    }
+
+    span {
+      color: ${({ theme }) => theme.gray};
+    }
+  }
+`;
+
+const LogoutButton = styled(Button)`
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.lightGray};
+  color: ${({ theme }) => theme.gray};
+  width: 100%;
+`;
 
 export default ProfilePage;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -5,6 +5,7 @@ import RegisterPage from '@/pages/auth/register';
 import CartPage from '@/pages/cart';
 import PizzaPage from '@/pages/pizza';
 import NotFoundPage from '@/pages/404';
+import ProfilePage from '@/pages/auth/profile';
 
 const AppRouter = () => {
   return (
@@ -14,6 +15,7 @@ const AppRouter = () => {
         <Route path='/home' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
         <Route path='/cart' element={<CartPage />} />
         <Route path='/pizza/:id' element={<PizzaPage />} />
         <Route path='*' element={<NotFoundPage />} />
